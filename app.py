8cad7bf3c938e8a4cdcfbd45801449138df113dc
Olivia Golden <olivia.golden.w@gmail.com>
2023-12-09 14:44:17 -0600

    app routes

--- app.py
+++ app.py
@@ -1,12 +1,68 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, render_template, redirect, request
+from models import db, connect_db, User
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "SECRET"
+
+debug = DebugToolbarExtension(app)
 
 connect_db(app)
 db.create_all()
+
+@app.route("/")
+def list_users():
+    """ List users and show add form. """
+
+    users = User.query.all()
+    return render_template("list.html", users=users)
+
+@app.route("/", methods=["GET", "POST"])
+def add_user():
+    """ Add new user and redirect to list """
+    if request.method == "GET":
+        return render_template("new.html")
+
+    if request.method == "POST":
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        image_url = request.form['image_url']
+
+        user = User(first_name=first_name, last_name=last_name, image_url=image_url)
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(f"/{user.id}")
+
+@app.route("/<int:user_id>")
+def user_detail(user_id):
+    """ Show info on a single user. """
+
+    user = User.query.get_or_404(user_id)
+    return render_template("detail.html", user=user)
+
+@app.route("/edit/<int:user_id>", methods=["GET", "POST"])
+def edit_user(user_id):
+    """ Edit a single user and return to user info """
+
+    user = User.query.get_or_404(user_id)
+
+    if request.method == "GET":
+        return redirect("edit.html", user=user)
+    
+    if request.method == "POST":
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        image_url = request.form['image_url']
+
+        user.first_name = first_name
+        user.last_name = last_name
+        user.image_url = image_url
+        db.session.commit()
+
+        return redirect(f"/{user.id}")
