6459b3ae36a230ee6f7e8a244fcf0b6d859c440c
Olivia Golden <olivia.golden.w@gmail.com>
2023-12-09 12:44:26 -0600

    user model

--- models.py
+++ models.py
@@ -1,1 +1,25 @@
 """Models for Blogly."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    """ Connect to database """
+
+    db.app = app
+    db.init_app(app)
+
+class User(db.Model):
+    """ A User has id, first name, last name, and profile image url. """
+
+    id = db.Column(db.Integer,
+                   primary_key = True,
+                   autoincrement = True)
+    first_name = db.Column(db.String(50),
+                           nullable = False)
+    last_name = db.Column(db.String(50),
+                          nullable = False)
+    image_url = db.Column(db.String(100))
+
+
